78808daf64fefc5d894059c33aeddab4cb2b6ec5
halafi <filiphalas74@gmail.com>
2020-08-10 10:58:35 +0200

    build: ts-node

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   // setupFiles: ['./config/jestSetup.js'],
   // setupFilesAfterEnv: ['./etc/jestSetupFramework.js'],
-  testPathIgnorePatterns: ['/node_modules/', 'mocks'],
+  // testPathIgnorePatterns: ['/node_modules/', 'mocks'],
   // collectCoverageFrom: ['site/**/*.js', 'workers-site/**/*.js'],
   // coverageReporters: ['json', 'lcov', 'text', 'text-summary'],
-  testEnvironment: 'node',
 };
