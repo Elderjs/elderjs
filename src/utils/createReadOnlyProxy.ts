dcb97f81bd0bdf19a6a615a49d8414e881bdc9a5
halafi <filiphalas74@gmail.com>
2020-08-10 17:37:00 +0200

    chore: clean

--- src/utils/createReadOnlyProxy.ts
+++ src/utils/createReadOnlyProxy.ts
@@ -3,7 +3,7 @@ function createReadOnlyProxy(obj: object, objName: string, location: string) {
   try {
     if (typeof obj !== 'object' && !Array.isArray(obj)) return obj;
     return new Proxy(obj, {
-      set(target, prop, val) {
+      set() {
         console.log(
           `Object ${objName} is not mutable from ${location}. Check the error below for the hook/plugin that is attempting to mutate properties outside of the rules in hookInterface.ts or in other restricted areas.`,
         );
