f88869a2d2676d0dff6da9ede21e0861ff9189a5
halafi <filiphalas74@gmail.com>
2020-08-11 16:55:09 +0200

    fix: partialHydration

--- src/partialHydration/partialHydration.ts
+++ src/partialHydration/partialHydration.ts
@@ -3,13 +3,15 @@ const partialHydration = {
     // there are limitations to this regex. Like you can't have an object {key:{key:val}} as a prop.
     const matches = content.matchAll(/<([a-zA-Z]+)\s+hydrate-client={([^]*?})}/gim);
 
-    matches.forEach((match) => {
-      const componentName = match[1];
-      const dataObject = match[2];
+    if (matches.length) {
+      matches.forEach((match) => {
+        const componentName = match[1];
+        const dataObject = match[2];
 
-      const replacement = `<div class="needs-hydration" data-component="${componentName}"  data-data={JSON.stringify(${dataObject})}`;
-      content = content.replace(match[0], replacement);
-    });
+        const replacement = `<div class="needs-hydration" data-component="${componentName}"  data-data={JSON.stringify(${dataObject})}`;
+        content = content.replace(match[0], replacement);
+      });
+    }
 
     return { code: content };
   },
