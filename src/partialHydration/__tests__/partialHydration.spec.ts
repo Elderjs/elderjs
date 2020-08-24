77a58fe31bdd984070a912882b467a9c6d752743
Nick Reese <nick@nicholasreese.com>
2020-08-24 13:26:29 -0400

    test: 💍 Beef up tests for partial hydration

--- src/partialHydration/__tests__/partialHydration.spec.ts
+++ src/partialHydration/__tests__/partialHydration.spec.ts
@@ -10,6 +10,28 @@ test('#partialHydration', async () => {
   ).toEqual(
     `<div class="needs-hydration" data-hydrate-component="DatePicker"  data-hydrate-props={JSON.stringify({ a: "b" })} data-hydrate-options="{ "loading": "lazy" }" />`,
   );
+
+  expect(
+    (
+      await partialHydration.markup({
+        content: '<DatePicker hydrate-client={{ a: "b" }} hydrate-options={{ loading: "eager" }} />',
+      })
+    ).code,
+  ).toEqual(
+    `<div class="needs-hydration" data-hydrate-component="DatePicker"  data-hydrate-props={JSON.stringify({ a: "b" })} data-hydrate-options={JSON.stringify({ loading: "eager" })} />`,
+  );
+
+  expect(
+    (
+      await partialHydration.markup({
+        content:
+          '<DatePicker hydrate-client={{ a: "b" }} hydrate-options={{ loading: "eager", rootMargin: "500px", threshold: 0 }} />',
+      })
+    ).code,
+  ).toEqual(
+    `<div class="needs-hydration" data-hydrate-component="DatePicker"  data-hydrate-props={JSON.stringify({ a: "b" })} data-hydrate-options={JSON.stringify({ loading: "eager", rootMargin: "500px", threshold: 0 })} />`,
+  );
+
   expect(
     (
       await partialHydration.markup({
