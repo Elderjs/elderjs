8365b759c000a543a37e83bebbf85210d70edf24
Nick Reese <nick@nicholasreese.com>
2020-08-26 14:13:27 -0400

    fix: remove dead code

--- src/partialHydration/partialHydration.ts
+++ src/partialHydration/partialHydration.ts
@@ -1,31 +1,3 @@
-// const partialHydration = {
-//   markup: async ({ content /* , filename */ }) => {
-//     let input = content;
-//     // Note: this regex only supports self closing components.
-//     // Slots aren't supported for client hydration either.
-//     const matches = content.matchAll(/<([a-zA-Z]+)[^>]+hydrate-client={([^]*?})}[^/>]+\/>/gim);
-
-//     for (const match of matches) {
-//       const [wholeMatch, componentName, componentProps] = match;
-
-//       // check for hydrate options
-//       const re = /hydrate-options={([^]*?})}/gim;
-//       const hydrateOptionsResults = re.exec(match.input);
-
-//       let replacement = `<div class="needs-hydration" data-hydrate-component="${componentName}"  data-hydrate-props={JSON.stringify(${componentProps})}`;
-//       if (hydrateOptionsResults && hydrateOptionsResults[1] && hydrateOptionsResults[1].length > 0) {
-//         replacement += ` data-hydrate-options={JSON.stringify(${hydrateOptionsResults[1]})}`;
-//       } else {
-//         replacement += ` data-hydrate-options={JSON.stringify({ "loading": "lazy" })}`;
-//       }
-//       replacement += ` />`;
-//       input = input.replace(wholeMatch, replacement);
-//     }
-
-//     return { code: input };
-//   },
-// };
-
 const extractHydrateOptions = (htmlString) => {
   const hydrateOptionsPattern = /hydrate-options={([^]*?})}/gim;
 
