293dc3f70f1eb3eaf5e7980df18d799f5d9838e3
Nick Reese <nick@Nicks-MacBook-Pro.local>
2020-09-09 14:17:24 -0400

    fix: 🐛 Stacks were reversed processing 100-1 instead of 1-100

--- src/utils/prepareProcessStack.ts
+++ src/utils/prepareProcessStack.ts
@@ -4,13 +4,14 @@ function prepareProcessStack(page) {
     const str = page[name]
       .map((s) => ({ ...s, priority: s.priority || 50 }))
       .sort((a, b) => a.priority - b.priority)
-      .reduce((out, cv) => {
+      .reduce((out, cv, i, arr) => {
         if (page.settings.debug && page.settings.debug.stacks) {
+          console.log(`stack.${name}`, arr, i);
           console.log(`Adding to ${name} from ${cv.source}`);
           console.log(cv);
         }
         if (cv.string && cv.string.length > 0) {
-          out = `${cv.string}${out}`;
+          out = `${out}${cv.string}`;
         }
 
         return out;
