2e987560a84e66b5a0e8cfe8bb59b6c539caf2c9
halafi <filiphalas74@gmail.com>
2020-09-22 13:56:54 +0200

    test: hooks.spec.ts

--- src/__tests__/hooks.spec.ts
+++ src/__tests__/hooks.spec.ts
@@ -2,6 +2,21 @@ import hooks from '../hooks';
 
 jest.mock('../externalHelpers', () => () => Promise.resolve({ permalink: jest.fn() }));
 
+jest.mock('../utils/prepareShortcodeParser', () => ({ customJsStack, cssStack, headStack }) => ({
+  parse: (templateHtml) => {
+    customJsStack.push('console.log("test");');
+    cssStack.push('body{display:none;}');
+    headStack.push('<title>Hello</title>');
+    return Promise.resolve(`..${templateHtml}..`);
+  },
+}));
+
+jest.mock('../utils/Page', () => {
+  return jest.fn(() => ({
+    html: () => Promise.resolve(`<html>new page</html>`),
+  }));
+});
+
 process.cwd = () => 'test';
 
 jest.mock('path', () => ({
@@ -30,11 +45,73 @@ describe('#hooks', () => {
     const hook = hooks.find((h) => h.name === 'elderAddExternalHelpers');
     expect(await hook.run({ helpers: { old: jest.fn() }, query: {}, settings: {} })).toMatchSnapshot();
   });
+  it('elderExpressLikeMiddleware', async () => {
+    const hook = hooks.find((h) => h.name === 'elderExpressLikeMiddleware');
+    const next = () => 'next() was called';
+    const settings = {
+      server: {
+        prefix: '/dev',
+      },
+    };
+    // prefix not found
+    expect(
+      await hook.run({
+        next,
+        req: { path: '/' },
+        settings,
+      }),
+    ).toEqual('next() was called');
+    const headers = [];
+    const end = jest.fn();
+    // route found with slash added
+    expect(
+      await hook.run({
+        next,
+        req: { path: '/dev' },
+        res: {
+          headerSent: false,
+          setHeader: (key, val) => {
+            headers.push(`${key}-${val}`);
+          },
+          end,
+        },
+        routes: {
+          Home: {
+            data: { foo: 'bar' },
+          },
+        },
+        serverLookupObject: {
+          '/dev/': {
+            route: 'Home',
+          },
+        },
+        settings,
+      }),
+    ).toEqual({});
+    expect(end).toHaveBeenCalledTimes(1);
+    expect(headers).toEqual(['Content-Type-text/html']);
+  });
   it('elderProcessShortcodes', async () => {
-    const hook = hooks.find((h) => h.name === 'elderAddExternalHelpers');
-    expect(await hook.run({ helpers: { old: jest.fn() }, query: {}, settings: {} })).toMatchSnapshot();
-
-    // TODO!!!
+    const hook = hooks.find((h) => h.name === 'elderProcessShortcodes');
+    const headStack = [];
+    const cssStack = [];
+    const customJsStack = [];
+    expect(
+      await hook.run({
+        headStack,
+        cssStack,
+        customJsStack,
+        templateHtml: '<html>hi</html>',
+        helpers: {},
+        query: {},
+        settings: {},
+      }),
+    ).toEqual({
+      cssStack: ['body{display:none;}'],
+      customJsStack: ['console.log("test");'],
+      headStack: ['<title>Hello</title>'],
+      templateHtml: '..<html>hi</html>..',
+    });
   });
   it('elderAddMetaCharsetToHead', async () => {
     const hook = hooks.find((h) => h.name === 'elderAddMetaCharsetToHead');
