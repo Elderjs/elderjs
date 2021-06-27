96b504cc385279c4d7ab5bccddb0df4b6d89aa6b
Nick Reese <code@nicholasreese.com>
2021-06-27 17:27:30 -0400

    test: 💍 update tests

--- src/partialHydration/__tests__/hydrateComponents.spec.ts
+++ src/partialHydration/__tests__/hydrateComponents.spec.ts
@@ -78,21 +78,14 @@ describe('#hydrateComponents', () => {
     expect(howManyBytes('{wtf}')).toBe(5);
   });
 
-  describe('#hydrateConpentsCore', () => {
+  describe('#hydrateComponentsCore', () => {
     test('hydrates a components', () => {
       const page = JSON.parse(JSON.stringify(defaultPage));
       page.componentsToHydrate = JSON.parse(JSON.stringify(defaultComponents));
 
       hydrateComponents(page);
-      expect(page.beforeHydrateStack).toHaveLength(1);
-      expect(page.hydrateStack).toHaveLength(3);
-    });
-
-    test('adds default decompress code', () => {
-      const page = JSON.parse(JSON.stringify(defaultPage));
-      page.componentsToHydrate = JSON.parse(JSON.stringify(defaultComponents));
-      hydrateComponents(page);
-      expect(page.beforeHydrateStack[0].string).toEqual('<script>$ejs = function(_ejs){return _ejs}</script>');
+      expect(page.beforeHydrateStack).toHaveLength(0);
+      expect(page.hydrateStack).toHaveLength(1);
     });
 
     test('adds custom decompress code on propCompression', async () => {
@@ -106,24 +99,9 @@ describe('#hydrateComponents', () => {
       };
 
       await hydrateComponents(page);
-      expect(page.beforeHydrateStack[0].string).toEqual(`<script>
-      var $ejs = function(){
-        var gt = function (_ejs) { return Object.prototype.toString.call(_ejs).slice(8, -1);};
-        var ejs = new Map([["1","b"],["2","c"],["3","a"]]);
-         return function(_ejs){
-            if (ejs.has(_ejs)) return ejs.get(_ejs);
-            if (Array.isArray(_ejs)) return _ejs.map((t) => $ejs(t));
-            if (gt(_ejs) === "Object") {
-            return Object.keys(_ejs).reduce(function (out, cv){
-                var key = ejs.get(cv) || cv;
-                out[key] = $ejs(_ejs[cv]);
-                return out;
-              }, {});
-            }
-            return _ejs;
-        };
-      }();
-    </script>`);
+      expect(
+        page.hydrateStack[0].string.replace(/\s/g, '').indexOf(`constejs=newMap([["1","b"],["2","c"],["3","a"]]);`),
+      ).toBeGreaterThan(-1);
     });
 
     test('preloads without external prop file', async () => {
