e6e9f75c7ea352d54198607712bd9189c6bcad62
Nick Reese <code@nicholasreese.com>
2022-06-14 17:16:58 +0200

    code cleanup

--- src/core/getWebsocket.ts
+++ src/core/getWebsocket.ts
@@ -9,19 +9,8 @@ export type WSData = {
 export default function getWebsocket() {
   const server = createServer();
   const wss = new WebSocketServer({ server });
-
-  wss.on('connection', function connection(ws) {
-    ws.on('open', () => {
-      ws.send('hi');
-    });
-    // ws.on('message', function message(data) {
-    // console.log('received: %s', data);
-    // });
-  });
-
   server.listen(0);
 
-  console.log('address', wss.address());
   return {
     send: function sendData(data: WSData) {
       wss.clients.forEach((client) => {
