e9bae301bb85afe8f163e1ff7756596b0746fe30
Nick Reese <code@nicholasreese.com>
2022-06-16 12:23:15 +0200

    log errors in user helpers

--- src/externalHelpers.ts
+++ src/externalHelpers.ts
@@ -1,7 +1,5 @@
-/* eslint-disable global-require */
-/* eslint-disable import/no-dynamic-require */
 import path from 'path';
-import fs from 'fs';
+import fs from 'fs-extra';
 
 import { QueryOptions, SettingsOptions, THelpers } from './utils/types.js';
 
@@ -21,17 +19,19 @@ async function externalHelpers({
   const srcHelpers = path.join(settings.srcDir, 'helpers/index.js');
   try {
     if (!cache) {
-      try {
-        fs.statSync(srcHelpers);
-        const reqHelpers = await import(srcHelpers);
-        userHelpers = reqHelpers.default || reqHelpers;
+      if (fs.existsSync(srcHelpers)) {
+        try {
+          const reqHelpers = await import(srcHelpers);
+          userHelpers = reqHelpers.default || reqHelpers;
 
-        if (typeof userHelpers === 'function') {
-          userHelpers = await userHelpers({ settings, query, helpers });
+          if (typeof userHelpers === 'function') {
+            userHelpers = await userHelpers({ settings, query, helpers });
+          }
+
+          cache = userHelpers;
+        } catch (err) {
+          console.error(err);
         }
-        cache = userHelpers;
-      } catch (err) {
-        // silence, helpers are optional
       }
     } else {
       userHelpers = cache;
