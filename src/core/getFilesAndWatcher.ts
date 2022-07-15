039f1ffd32ad5335515546c76fddebfcbef74514
Nick Reese <code@nicholasreese.com>
2022-07-15 12:32:42 +0200

    fix: bump, make sure to get changes on builds after esbuild finishes

--- src/core/getFilesAndWatcher.ts
+++ src/core/getFilesAndWatcher.ts
@@ -70,89 +70,87 @@ export default function getFilesAndWatcher(settings: TGetFilesAndWatcher): {
     client: all.filter((f) => f.includes(settings.clientComponents)),
   };
 
-  if (!settings.production && settings.server) {
-    // todo: add in plugin folders for Elder.js
-    const chok = chokidar.watch(paths, { alwaysStat: true, usePolling: true });
+  // todo: add in plugin folders for Elder.js
+  const chok = chokidar.watch(paths, { alwaysStat: true, usePolling: true });
 
-    // these are unhashed.
-    const chokFiles: Map<string, Stats> = new Map();
+  // these are unhashed.
+  const chokFiles: Map<string, Stats> = new Map();
 
-    // eslint-disable-next-line no-inner-declarations
-    function handleChange(file: string) {
-      const fixedFile = windowsPathFix(file);
-      const f = path.relative(settings.srcDir, file);
-      if (f.startsWith('routes') && !f.endsWith('svelte')) {
-        if (f.endsWith('route.js')) {
-          if (!files.routes.includes(file)) files.routes.push(file);
-          watcher.emit('route', hashUrl(file));
-        } else {
-          // find nearest route.
-          const routePaths = files.routes.map((r) => unhashUrl(r).replace('route.js', ''));
-          const found = routePaths.find((r) => file.includes(r));
-          if (found) {
-            watcher.emit('route', hashUrl(`${found}route.js`));
-          }
-        }
-      } else if (f.endsWith('.css') && f.includes('svelte-')) {
-        files.publicCssFile = makeCssRelative({
-          file,
-          distElder: settings.distDir,
-        });
-        watcher.emit('publicCssFile', files.publicCssFile);
-      } else if (f === 'hooks.js') {
-        watcher.emit('hooks', hashUrl(file));
-      } else if (f === 'shortcodes.js') {
-        watcher.emit('shortcodes', hashUrl(file));
-      } else if (file.includes(settings.ssrComponents)) {
-        const idx = files.server.findIndex((f) => f.includes(fixedFile));
-        files.server[idx] = hashUrl(fixedFile);
-        watcher.emit('ssr', hashUrl(fixedFile));
-      } else if (file.includes(settings.clientComponents)) {
-        const withoutHash = fixedFile.split('.')[0];
-        const idx = files.client.findIndex((f) => f.includes(withoutHash));
-        files.client[idx] = fixedFile;
-        watcher.emit('client', fixedFile);
-      } else if (file.endsWith(`elder.config.js`) || file.endsWith(`elder.config.cjs`)) {
-        watcher.emit('elder.config', hashUrl(file));
-        // } else if (file.endsWith('.css')) {
-        // watcher.emit('otherCssFile', makeCssRelative({ file, distElder: settings.distDir }));
-      } else if (file.endsWith(`${settings.srcDir}/helpers/index.js`)) {
-        watcher.emit('helpers', hashUrl(file));
+  // eslint-disable-next-line no-inner-declarations
+  function handleChange(file: string) {
+    const fixedFile = windowsPathFix(file);
+    const f = path.relative(settings.srcDir, file);
+    if (f.startsWith('routes') && !f.endsWith('svelte')) {
+      if (f.endsWith('route.js')) {
+        if (!files.routes.includes(file)) files.routes.push(file);
+        watcher.emit('route', hashUrl(file));
       } else {
-        // console.log('other file', file);
+        // find nearest route.
+        const routePaths = files.routes.map((r) => unhashUrl(r).replace('route.js', ''));
+        const found = routePaths.find((r) => file.includes(r));
+        if (found) {
+          watcher.emit('route', hashUrl(`${found}route.js`));
+        }
       }
-
-      if (file.includes(settings.srcDir)) {
-        watcher.emit('srcChange', file);
-      }
-
-      files.all = [...chokFiles.keys()];
+    } else if (f.endsWith('.css') && f.includes('svelte-')) {
+      files.publicCssFile = makeCssRelative({
+        file,
+        distElder: settings.distDir,
+      });
+      watcher.emit('publicCssFile', files.publicCssFile);
+    } else if (f === 'hooks.js') {
+      watcher.emit('hooks', hashUrl(file));
+    } else if (f === 'shortcodes.js') {
+      watcher.emit('shortcodes', hashUrl(file));
+    } else if (file.includes(settings.ssrComponents)) {
+      const idx = files.server.findIndex((f) => f.includes(fixedFile));
+      files.server[idx] = hashUrl(fixedFile);
+      watcher.emit('ssr', hashUrl(fixedFile));
+    } else if (file.includes(settings.clientComponents)) {
+      const withoutHash = fixedFile.split('.')[0];
+      const idx = files.client.findIndex((f) => f.includes(withoutHash));
+      files.client[idx] = fixedFile;
+      watcher.emit('client', fixedFile);
+    } else if (file.endsWith(`elder.config.js`) || file.endsWith(`elder.config.cjs`)) {
+      watcher.emit('elder.config', hashUrl(file));
+      // } else if (file.endsWith('.css')) {
+      // watcher.emit('otherCssFile', makeCssRelative({ file, distElder: settings.distDir }));
+    } else if (file.endsWith(`${settings.srcDir}/helpers/index.js`)) {
+      watcher.emit('helpers', hashUrl(file));
+    } else {
+      // console.log('other file', file);
     }
 
-    let initialScanComplete = false;
-    chok.on('add', (file, stat) => {
-      chokFiles.set(file, stat);
-      if (initialScanComplete) {
-        handleChange(file);
-      }
-    });
+    if (file.includes(settings.srcDir)) {
+      watcher.emit('srcChange', file);
+    }
 
-    chok.on('change', (file, stat) => {
-      if (!chokFiles.has(file) || chokFiles.get(file).size !== stat.size) {
-        chokFiles.set(file, stat);
-        handleChange(file);
-      }
-    });
-
-    chok.on('unlink', (file) => {
-      chokFiles.delete(file);
-      if (file === files.publicCssFile) files.publicCssFile = '';
-    });
-
-    chok.on('ready', () => {
-      initialScanComplete = true;
-    });
+    files.all = [...chokFiles.keys()];
   }
+
+  let initialScanComplete = false;
+  chok.on('add', (file, stat) => {
+    chokFiles.set(file, stat);
+    if (initialScanComplete) {
+      handleChange(file);
+    }
+  });
+
+  chok.on('change', (file, stat) => {
+    if (!chokFiles.has(file) || chokFiles.get(file).size !== stat.size) {
+      chokFiles.set(file, stat);
+      handleChange(file);
+    }
+  });
+
+  chok.on('unlink', (file) => {
+    chokFiles.delete(file);
+    if (file === files.publicCssFile) files.publicCssFile = '';
+  });
+
+  chok.on('ready', () => {
+    initialScanComplete = true;
+  });
 
   return { files, watcher };
 }
